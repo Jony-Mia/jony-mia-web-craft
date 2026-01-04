7013b893325b801c04678a34802c78bf7ceeadfe
Jony-Mia <157081064+Jony-Mia@users.noreply.github.com>
2026-01-04 19:37:36 +0600

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 
 // https://vitejs.dev/config/
@@ -10,11 +9,8 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-<<<<<<< HEAD
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-=======
+
   plugins: [react(), mode === "development"].filter(Boolean),
->>>>>>> 2885f632fd8de9fbd7e0409bef73fcfc2b54fc27
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
